a70535104bf53e6f476d8e65e62c02fb18e3866f
MasterUNG <phrombutr@gmail.com>
2018-01-27 15:57:38 +0700

    Alert OK

--- app/src/main/java/masterung/androidthai/in/th/ungchat/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungchat/fragment/RegisterFragment.java
@@ -3,17 +3,105 @@ package masterung.androidthai.in.th.ungchat.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import masterung.androidthai.in.th.ungchat.MainActivity;
 import masterung.androidthai.in.th.ungchat.R;
+import masterung.androidthai.in.th.ungchat.utility.MyAlert;
 
 /**
  * Created by masterung on 27/1/2018 AD.
  */
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Toolbar Controller
+        toolbarController();
+
+
+    }   // Main Method
+
+    private void toolbarController() {
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register_th));
+
+//        Setup Navigator
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+//        Setup Menu
+        setHasOptionsMenu(true);
+
+
+    }   // toolbar
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_register, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemRegister) {
+
+            uploadValueToFirebase();
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToFirebase() {
+
+//        Initial View
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText emailEditText = getView().findViewById(R.id.edtEmail);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String emailString = emailEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+        if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty()) {
+
+//            Have Space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space",
+                    "Please Fill All Every Blank");
+
+        } else {
+
+//            No Space
+
+
+        }
+
+
+    }
 
     @Nullable
     @Override
@@ -21,4 +109,4 @@ public class RegisterFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
     }
-}
+}   // Main Class
---
+++ app/src/main/java/masterung/androidthai/in/th/ungchat/utility/MyAlert.java
@@ -0,0 +1,40 @@
+package masterung.androidthai.in.th.ungchat.utility;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import masterung.androidthai.in.th.ungchat.R;
+
+/**
+ * Created by masterung on 27/1/2018 AD.
+ */
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void normalDialog(String titleString, String messageString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_alert);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }
+
+
+}   // Main Class
