dccafd69638bc298c81a34f1a0400bd15b09b349
MasterUNG <phrombutr@gmail.com>
2018-01-28 14:18:22 +0700

    Add ChatFragment Success

--- app/src/main/java/masterung/androidthai/in/th/ungchat/ServiceActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/ungchat/ServiceActivity.java
@@ -3,12 +3,25 @@ package masterung.androidthai.in.th.ungchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.ungchat.fragment.ChatFragment;
+import masterung.androidthai.in.th.ungchat.fragment.RegisterFragment;
+
 public class ServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentServiceFragment, new ChatFragment())
+                    .commit();
+
+        }
+
+
     }   // Main Method
 
 }   // Main Class
