0e6088b0dfc08c946ee997443c7a7d765e02e958
MasterUNG <phrombutr@gmail.com>
2018-01-28 14:05:08 +0700

    Drawer OK

---
+++ app/src/main/java/masterung/androidthai/in/th/ungchat/fragment/ChatFragment.java
@@ -0,0 +1,24 @@
+package masterung.androidthai.in.th.ungchat.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterung.androidthai.in.th.ungchat.R;
+
+/**
+ * Created by masterung on 28/1/2018 AD.
+ */
+
+public class ChatFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        return view;
+    }
+}
